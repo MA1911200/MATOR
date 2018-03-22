d384f6509028d7b749515aba57bd4ef9dd943df6
MA1911200 <32337733+MA1911200@users.noreply.github.com>
2018-03-22 17:08:05 +0100

    Add files via upload

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,6 +1,3 @@
-from termcolor import colored
-
-print colored('hello', 'red'), colored('world', 'green')
 print(" ")
 print("  ███╗   ███╗ █████╗ ████████╗ ██████╗ ██████╗ ")
 print("  ████╗ ████║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗")
