ce983b87a826db018ba8305be75e605cfe2dbe1c
MA1911200 <32337733+MA1911200@users.noreply.github.com>
2018-05-12 22:57:33 +0200

    Add files via upload

---
+++ MATOR-Main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os
+import time
+
+# Colorama
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+# Main Menu
+os.system("cls||clear")
+print(Fore.RED + " .: MATOR Password Generator :. " + Style.RESET_ALL)
+print(" Please fill the Information below to Generate the Passwords... ")
+print("")
+
+print(" [!] Account Username                                  " + Fore.GREEN + "[ DONE ]" + Style.RESET_ALL)
+input(" >> ")
